5129011fd166c5c32b0bc8e2ebb57e64088b234d
Rachid-Gharib <73350230+Rachid-Gharib@users.noreply.github.com>
2022-01-14 13:08:18 -0500

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Projet_Web_MVC_avec_fichier___CRUD {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projet_Web_MVC_avec_fichier___CRUD.MvcApplication" Language="C#" %>
